f9fbcf1be03fd932b2c7cf1971cc697939e88894
Joseph Rivera <JRivera@Trellance.com>
2019-02-22 15:05:12 -0500

    registeration route and form added as well as login

--- mini_social_app/flaskblog.py
+++ mini_social_app/flaskblog.py
@@ -1,20 +1,25 @@
-from flask import Flask, render_template, url_for
+
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '43e51957d620f4d0b0228b77ed60be59'
 
 posts = [
     {
-        'author':'cory schafer',
-        'title':'blog post 1',
-        'content':'some content',
-        'date_posted':'April 20, 2018'
+        'author': 'Corey Schafer',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
     },
-        {
-        'author':'Jake Bamforth',
-        'title':'blog post 2',
-        'content':'some content',
-        'date_posted':'April 24, 2018'
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'
     }
 ]
+
 
 @app.route("/")
 @app.route("/home")
@@ -24,7 +29,29 @@ def home():
 
 @app.route("/about")
 def about():
-    return render_template('about.html', title='About The Canteen')
+    return render_template('about.html', title='About')
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
